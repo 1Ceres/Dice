272c0ab9d811996004bf95cb2b8de1abd3aebf0e
Alan K <alkowebdeveloper@gmail.com>
2022-03-09 08:32:08 +0000

    linkin git and github

---
+++ index.js
@@ -0,0 +1,13 @@
+var p1 = (Math.floor(Math.random()*6)+1);
+var p2 = (Math.floor(Math.random()*6)+1);
+document.querySelector(".img1").src = "images/dice"+p1+".png";
+document.querySelector(".img2").src = "images/dice"+p2+".png";
+if (p1>p2){
+  document.querySelector("a").innerHTML = "<a href='dicee.html'>Player 1 Won!</a>";
+}
+else if (p2>p1){
+    document.querySelector("a").innerHTML = "<a href='dicee.html'>Player 2 Won!</a>";
+}
+else{
+  document.querySelector("a").innerHTML = "<a href='dicee.html'>Draw!!</a>";
+}
