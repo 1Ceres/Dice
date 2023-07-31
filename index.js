406d85027892904beca9918d306802a2d11d09bb
1Ceres <96084816+1Ceres@users.noreply.github.com>
2023-07-31 19:31:03 +0100

    Update index.js
    update index.js with "index.html" name of home page

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var p2 = (Math.floor(Math.random()*6)+1);
 document.querySelector(".img1").src = "images/dice"+p1+".png";
 document.querySelector(".img2").src = "images/dice"+p2+".png";
 if (p1>p2){
-  document.querySelector("a").innerHTML = "<a href='dicee.html'>Player 1 Won!</a>";
+  document.querySelector("a").innerHTML = "<a href='index.html'>Player 1 Won!</a>";
 }
 else if (p2>p1){
-    document.querySelector("a").innerHTML = "<a href='dicee.html'>Player 2 Won!</a>";
+    document.querySelector("a").innerHTML = "<a href='index.html'>Player 2 Won!</a>";
 }
 else{
-  document.querySelector("a").innerHTML = "<a href='dicee.html'>Draw!!</a>";
+  document.querySelector("a").innerHTML = "<a href='index.html'>Draw!!</a>";
 }
